51973ab07eedeca9052abc1470c69578d895dde8
mlatinov <latinovmetodi@gmail.com>
2025-04-12 23:52:02 +0300

    # model tuning # add features

--- scripts/modeling.R
+++ scripts/modeling.R
@@ -7,6 +7,7 @@ library(doFuture)
 library(future)
 library(patchwork)
 library(finetune)
+library(scimo)
 tidymodels_prefer()
 
 # Env Spec
@@ -29,65 +30,138 @@ validation_set <- testing(split)
 ## Create a recipe with 4 PCA groups for better interpretability
 recipe_pca_4 <- recipe(yield ~ .,data = model_training) %>%
   
+  # Isolate ID
+  update_role(id, new_role = "ID") %>%  
+  
+  # Create new Features
+  step_mutate(
+    
+    # Add Pollinator-related features
+    total_polinators = honeybee + bumbles + andrena + osmia,
+    wild_dom_ratio = (bumbles + andrena + osmia) / (honeybee + 1),
+    
+    # Temperature Features
+    average_temp_range = (AverageOfUpperTRange + AverageOfLowerTRange) / 2,
+    temp_stability = (MaxOfUpperTRange - MinOfUpperTRange) + (MaxOfLowerTRange - MinOfLowerTRange),
+    
+    # Rain Patterns
+    rain_anomaly =  RainingDays - AverageRainingDays,
+    rain_per_pollinator = RainingDays / (total_polinators + 1),
+    
+    # Crop Features
+    mass_per_seed = fruitmass / (seeds + 1),
+    seed_efficiency = seeds / (fruitmass + 1),
+    clonesize_temp_diff = clonesize / (AverageOfUpperTRange - AverageOfLowerTRange + 1)
+    ) %>%
+  
+  # Remove old features 
+  step_rm(removals = c(
+    "MaxOfUpperTRange","MaxOfLowerTRange","MinOfLowerTRange","upper_temp_range","MinOfUpperTRange",
+    "lower_temp_range","honeybee","bumbles","andrena","osmia","RainingDays","AverageRainingDays",
+    "fruitmass","AverageOfLowerTRange","AverageOfUpperTRange","clonesize")) %>%
+  
   # Remove near zero var features
   step_nzv(all_predictors()) %>%
   
   # Apply Yeo-Jonson transformation
   step_YeoJohnson(all_predictors()) %>%
   
-  # Normalize the features
-  step_normalize(all_predictors()) %>%
+  # Center and Scale
+  step_center(all_numeric_predictors()) %>%
+  step_scale(all_numeric_predictors()) %>%
   
-  # PCA for fruit related features
-  step_pca(starts_with("fruit") | contains("seeds"),prefix = "pca_fruit",num_comp = tune("fruit_pca"),id = "fruit_pca") %>%
+  # PCA for fruit related Features
+  step_pca(columns = c("mass_per_seed","seed_efficiency","clonesize_temp_diff"),prefix = "pca_fruit",num_comp = tune("fruit_pca"),id = "fruit_pca") %>%
   
-  # PCA for Temperature-related variables
-  step_pca(starts_with("AverageOf") | contains("TRange"),prefix = "pca_temp",num_comp = tune("temp_pca"),id = "temp_pca") %>%
+  # PCA for Temperature-related Feaures
+  step_pca(columns = c(" average_temp_range","temp_stability"),prefix = "pca_temp",num_comp = tune("temp_pca"),id = "temp_pca") %>%
   
-  # PCA for Bee related featues
-  step_pca(columns = c("bumbles", "andrena", "osmia", "honeybee"),prefix = "pca_bee",num_comp = tune("bee_pca"),id = "bee_pca") %>%
+  # PCA for Bee related Features
+  step_pca(columns = c("total_polinators","wild_dom_ratio"),prefix = "pca_bee",num_comp = tune("bee_pca"),id = "bee_pca") %>%
   
-  # PCA for Rain Related Featues 
-  step_pca(c("RainingDays", "AverageRainingDays"),prefix = "pca_rain_",num_comp = tune("rain_pca"),id = "rain_pca") 
+  # PCA for Rain Related Features
+  step_pca(columns = c("rain_anomaly", "rain_per_pollinator"),prefix = "pca_rain_",num_comp = tune("rain_pca"),id = "rain_pca") 
   
-# Extract the params and update the number of components
-params_4_pca <- parameters(recipe_pca_4) %>%
-  update(
-    fruit_pca = num_comp(range = c(1,3)),
-    temp_pca  = num_comp(range = c(1,4)),
-    bee_pca = num_comp(range = c(1,2)),
-    rain_pca = num_comp(range = c(1,2))
-  )
-
-# Create a grid
-grid_4_pca <- grid_space_filling(params_4_pca,size  = 10)
 
 ## Create a recipe with one PCA for performance
 recipe_pca_1 <- recipe(yield ~ .,data = model_training) %>%
   
+  # Isolate ID
+  update_role(id, new_role = "ID") %>% 
+  
+  # Create new Features
+  step_mutate(
+    
+    # Add Pollinator-related features
+    total_polinators = honeybee + bumbles + andrena + osmia,
+    wild_dom_ratio = (bumbles + andrena + osmia) / (honeybee + 1),
+    
+    # Temperature Features
+    average_temp_range = (AverageOfUpperTRange + AverageOfLowerTRange) / 2,
+    temp_stability = (MaxOfUpperTRange - MinOfUpperTRange) + (MaxOfLowerTRange - MinOfLowerTRange),
+    
+    # Rain Patterns
+    rain_anomaly =  RainingDays - AverageRainingDays,
+    rain_per_pollinator = RainingDays / (total_polinators + 1),
+    
+    # Crop Features
+    mass_per_seed = fruitmass / (seeds + 1),
+    seed_efficiency = seeds / (fruitmass + 1),
+    clonesize_temp_diff = clonesize / (AverageOfUpperTRange - AverageOfLowerTRange + 1)
+  ) %>%
+  
+  # Remove old features 
+  step_rm(removals =  c(
+    "MaxOfUpperTRange","MaxOfLowerTRange","MinOfLowerTRange","upper_temp_range","MinOfUpperTRange",
+    "lower_temp_range","honeybee","bumbles","andrena","osmia","RainingDays","AverageRainingDays",
+    "fruitmass","AverageOfLowerTRange","AverageOfUpperTRange","clonesize")) %>%
+  
   # Remove near zero var features
   step_nzv(all_predictors()) %>%
   
   # Apply Yeo-Johnson transformation
   step_YeoJohnson(all_predictors()) %>%
   
-  # Normalize the features
-  step_normalize(all_predictors()) %>%
+  # Center and Scale
+  step_center(all_numeric_predictors()) %>%
+  step_scale(all_numeric_predictors()) %>%
   
   # PCA 
   step_pca(all_predictors(),num_comp = tune("all_pca"),id = "all_pca")
   
-# Extract params and update the number of components
-params_1_pca <- parameters(recipe_pca_1) %>%
-  update(
-    all_pca = num_comp(range = c(1,10))
-  )
-
-# Create a grid 
-grid_pca_1 <- grid_space_filling(params_1_pca,size = 10)
 
 ## Create a recipe with one PLS for performance
 recipe_pls_1 <- recipe(yield ~ .,data = model_training) %>%
+  
+  # Isolate ID
+  update_role(id, new_role = "ID") %>% 
+  
+  # Create new Features
+  step_mutate(
+    
+    # Add Pollinator-related features
+    total_polinators = honeybee + bumbles + andrena + osmia,
+    wild_dom_ratio = (bumbles + andrena + osmia) / (honeybee + 1),
+    
+    # Temperature Features
+    average_temp_range = (AverageOfUpperTRange + AverageOfLowerTRange) / 2,
+    temp_stability = (MaxOfUpperTRange - MinOfUpperTRange) + (MaxOfLowerTRange - MinOfLowerTRange),
+    
+    # Rain Patterns
+    rain_anomaly =  RainingDays - AverageRainingDays,
+    rain_per_pollinator = RainingDays / (total_polinators + 1),
+    
+    # Crop Features
+    mass_per_seed = fruitmass / (seeds + 1),
+    seed_efficiency = seeds / (fruitmass + 1),
+    clonesize_temp_diff = clonesize / (AverageOfUpperTRange - AverageOfLowerTRange + 1)
+  ) %>%
+  
+  # Remove old features 
+  step_rm(removals =  c(
+    "MaxOfUpperTRange","MaxOfLowerTRange","MinOfLowerTRange","upper_temp_range","MinOfUpperTRange",
+    "lower_temp_range","honeybee","bumbles","andrena","osmia","RainingDays","AverageRainingDays",
+    "fruitmass","AverageOfLowerTRange","AverageOfUpperTRange","clonesize")) %>%
   
   # Remove near zero var features
   step_nzv(all_predictors()) %>%
@@ -95,14 +169,102 @@ recipe_pls_1 <- recipe(yield ~ .,data = model_training) %>%
   # Apply Yeo-Johnson transformation
   step_YeoJohnson(all_predictors()) %>%
   
-  # Normalize the features
-  step_normalize(all_predictors()) %>%
+  # Center and Scale
+  step_center(all_numeric_predictors()) %>%
+  step_scale(all_numeric_predictors()) %>%
   
   # PLS 
   step_pls(all_predictors(),outcome ="yield",num_comp = tune("all_pls"),id = "all_pls")
 
+
 ## Create a recipe with 4 groups pls
 recipe_pls_4 <- recipe(yield ~ .,data = model_training) %>%
+  
+  # Isolate ID
+  update_role(id, new_role = "ID") %>% 
+
+  # Create new Features
+  step_mutate(
+    
+    # Add Pollinator-related features
+    total_polinators = honeybee + bumbles + andrena + osmia,
+    wild_dom_ratio = (bumbles + andrena + osmia) / (honeybee + 1),
+    
+    # Temperature Features
+    average_temp_range = (AverageOfUpperTRange + AverageOfLowerTRange) / 2,
+    temp_stability = (MaxOfUpperTRange - MinOfUpperTRange) + (MaxOfLowerTRange - MinOfLowerTRange),
+    
+    # Rain Patterns
+    rain_anomaly =  RainingDays - AverageRainingDays,
+    rain_per_pollinator = RainingDays / (total_polinators + 1),
+    
+    # Crop Features
+    mass_per_seed = fruitmass / (seeds + 1),
+    seed_efficiency = seeds / (fruitmass + 1),
+    clonesize_temp_diff = clonesize / (AverageOfUpperTRange - AverageOfLowerTRange + 1)
+  ) %>%
+  
+  # Remove old features 
+  step_rm(removals =  c(
+    "MaxOfUpperTRange","MaxOfLowerTRange","MinOfLowerTRange","upper_temp_range","MinOfUpperTRange",
+    "lower_temp_range","honeybee","bumbles","andrena","osmia","RainingDays","AverageRainingDays",
+    "fruitmass","AverageOfLowerTRange","AverageOfUpperTRange","clonesize")) %>%
+
+  # Remove near zero var features
+  step_nzv(all_predictors()) %>%
+  
+  # Apply Yeo-Jonson transformation
+  step_YeoJohnson(all_predictors()) %>%
+  
+  # Center and Scale
+  step_center(all_numeric_predictors()) %>%
+  step_scale(all_numeric_predictors()) %>%
+  
+  # PCA for fruit related Features
+  step_pls(columns = c("mass_per_seed","seed_efficiency","clonesize_temp_diff"),prefix = "pls_fruit",num_comp = tune("fruit_pls"),id = "fruit_pca",outcome ="yield") %>%
+  
+  # PCA for Temperature-related Feaures
+  step_pls(columns = c(" average_temp_range","temp_stability"),prefix = "pls_temp",num_comp = tune("temp_pls"),id = "temp_pls",outcome ="yield") %>%
+  
+  # PCA for Bee related Features
+  step_pls(columns = c("total_polinators","wild_dom_ratio"),prefix = "pls_bee",num_comp = tune("bee_pls"),id = "bee_pls",outcome ="yield") %>%
+  
+  # PCA for Rain Related Features
+  step_pls(columns = c("rain_anomaly", "rain_per_pollinator"),prefix = "pls_rain_",num_comp = tune("rain_pls"),id = "rain_pls",outcome ="yield")
+
+
+## Create a Hierarchical clustering recipe based on hclust
+recipe_hclust_2 <- recipe(yield ~ .,data = model_training) %>%
+  
+  # Isolate ID
+  update_role(id, new_role = "ID") %>% 
+  
+  # Create new Features
+  step_mutate(
+    
+    # Add Pollinator-related features
+    total_polinators = honeybee + bumbles + andrena + osmia,
+    wild_dom_ratio = (bumbles + andrena + osmia) / (honeybee + 1),
+    
+    # Temperature Features
+    average_temp_range = (AverageOfUpperTRange + AverageOfLowerTRange) / 2,
+    temp_stability = (MaxOfUpperTRange - MinOfUpperTRange) + (MaxOfLowerTRange - MinOfLowerTRange),
+    
+    # Rain Patterns
+    rain_anomaly =  RainingDays - AverageRainingDays,
+    rain_per_pollinator = RainingDays / (total_polinators + 1),
+    
+    # Crop Features
+    mass_per_seed = fruitmass / (seeds + 1),
+    seed_efficiency = seeds / (fruitmass + 1),
+    clonesize_temp_diff = clonesize / (AverageOfUpperTRange - AverageOfLowerTRange + 1)
+  ) %>%
+  
+  # Remove old features 
+  step_rm(removals =  c(
+    "MaxOfUpperTRange","MaxOfLowerTRange","MinOfLowerTRange","upper_temp_range","MinOfUpperTRange",
+    "lower_temp_range","honeybee","bumbles","andrena","osmia","RainingDays","AverageRainingDays",
+    "fruitmass","AverageOfLowerTRange","AverageOfUpperTRange","clonesize")) %>%
   
   # Remove near zero var features
   step_nzv(all_predictors()) %>%
@@ -110,20 +272,12 @@ recipe_pls_4 <- recipe(yield ~ .,data = model_training) %>%
   # Apply Yeo-Jonson transformation
   step_YeoJohnson(all_predictors()) %>%
   
-  # Normalize all features
-  step_normalize(all_predictors()) %>%
+  # Center and Scale
+  step_center(all_numeric_predictors()) %>%
+  step_scale(all_numeric_predictors()) %>%
   
-  # PLS for fruit related features
-  step_pls(starts_with("fruit") | contains("seeds"),prefix = "pls_fruit",num_comp = tune("fruit_pls"),id = "fruit_pls",outcome = "yield") %>%
-  
-  # PLS for Temperature-related variables
-  step_pls(starts_with("AverageOf") | contains("TRange"),prefix = "pls_temp",num_comp = tune("temp_pls"),id = "temp_pls",outcome = "yield") %>%
-  
-  # PLS for Bee related features
-  step_pls(columns = c("bumbles", "andrena", "osmia", "honeybee"),prefix = "pls_bee",num_comp = tune("bee_pls"),id = "bee_pls",outcome = "yield") %>%
-  
-  # PLS for Rain Related features
-  step_pls(c("RainingDays", "AverageRainingDays"),prefix = "pls_rain_",num_comp = tune("rain_pls"),id = "rain_pls",outcome = "yield") 
+  # Apply Hclust
+  step_aggregate_hclust(all_numeric_predictors(),n_clusters = 2,linkage_method = "ward.D2",dist_metric = "manhattan",fun_agg = mean)
 
 #### Model Spec ####
 
@@ -171,7 +325,7 @@ bagged_nn <- bag_mlp() %>%
 ## Tune the recipes
 
 # Set Metric
-metric <- metric_set(mae)
+metric <- metric_set(yardstick::mae)
 
 # Tune recipe func
 tune_pca <- function(recipe){
@@ -189,7 +343,7 @@ tune_pca <- function(recipe){
   tuned_results <- workflow_recipe_set %>%
     workflow_map(
     fn = "tune_race_anova",
-    resamples = vfold_cv(data = train,v = 5,strata = yield),
+    resamples = vfold_cv(data = validation_set,v = 10,strata = yield),
     grid = 20,
     control = control_race(save_pred = TRUE, parallel_over = "everything", verbose = TRUE),
     metrics = metric
@@ -244,12 +398,20 @@ workflow_light_tune <- workflow_set(
     pls_4 = pls_4_final
   ),
   models = list(
+    
+    ## Neural Networks Models
     mlp = mlp %>% update(hidden_units = tune() ,epochs = tune()),
     bagged_nn = bagged_nn %>% update(hidden_units = tune() ,epochs = tune()),
+    
+    ## Mars Models 
     mars_model = mars_model %>% update(num_terms = tune(),prod_degree = tune()),
     bagged_mars_model = bagged_mars_model %>% update(num_terms = tune(),prod_degree = tune()),
+    
+    ## Tree based models
     random_forest = rf_model %>% update(mtry = tune() ,trees = 300 ,min_n = tune()),
     xgb = xgb_model %>% update(mtry = tune() ,trees = tune() ,tree_depth = tune(),learn_rate = tune() ,loss_reduction =tune() ,sample_size = tune()),
+    
+    ## Control
     null_model = null_model),
   cross = TRUE)
 
@@ -257,7 +419,7 @@ workflow_light_tune <- workflow_set(
 tuned_models <- workflow_light_tune %>%
   workflow_map(
     fn = "tune_race_anova",
-    resamples = vfold_cv(data = validation_set, v = 5, strata = yield),
+    resamples = vfold_cv(data = validation_set, v = 10, strata = yield),
     grid = 15,
     control = control_race(save_pred = TRUE, verbose_elim = TRUE, parallel_over = "everything"),
     metrics = metric
@@ -275,34 +437,42 @@ best_params_func <- function(workflow_map,model_id,metric){
 }
 
 # Save the best params from tune_race_anova
-best_mlp <- best_params_func(workflow_map = tuned_models, model_id = "pls_4_mlp")
+best_mlp <- best_params_func(workflow_map = tuned_models,model_id ="pca_1_bagged_nn")
 
-best_bagged_nn <- best_params_func(workflow_map = tuned_models,model_id ="pls_1_bagged_nn")
+best_bagged_nn <- best_params_func(workflow_map = tuned_models,model_id ="pca_1_bagged_nn")
 
 best_mars <- best_params_func(workflow_map = tuned_models,model_id ="pca_4_mars_model")
 
 best_bagged_mars_model <- best_params_func(workflow_map = tuned_models,model_id = "pca_4_bagged_mars_model")
 
-best_random_forest <- best_params_func(workflow_map = tuned_models,model_id = "pls_1_random_forest")
+best_random_forest <- best_params_func(workflow_map = tuned_models,model_id = "pca_1_random_forest")
 
 best_xgb <- best_params_func(workflow_map = tuned_models,model_id = "pca_4_xgb")
 
 # Workflow set Selection
 model_select_workflow_set <- workflow_set(
   preproc = list(
+    hculst_2 = recipe_hclust_2,
     pca_4 = pca_4_final,
     pca_1 = pca_1_final,
     pls_1 = pls_1_final,
     pls_4 = pls_4_final
     ),
   models = list(
+    
+    ## Neural Networks Models
     mlp = mlp %>% update(hidden_units = best_mlp$hidden_units ,epochs = best_mlp$epochs),
     bagged_nn = bagged_nn %>% update(hidden_units = best_bagged_nn$hidden_units ,epochs = best_bagged_nn$epochs),
+    
+    ## Mars Models 
     mars_model = mars_model %>% update(num_terms = best_mars$num_terms,prod_degree = best_mars$prod_degree),
     bagged_mars_model = bagged_mars_model %>% update(num_terms = best_bagged_mars_model$num_terms,prod_degree = best_bagged_mars_model$prod_degree),
+    
+    ## Tree based models
     random_forest = rf_model %>% update(mtry = best_random_forest$mtry ,trees = 300 ,min_n = best_random_forest$min_n),
     xgb = xgb_model %>% update(mtry = best_xgb$mtry,trees = best_xgb$trees,tree_depth = best_xgb$tree_depth,learn_rate = best_xgb$learn_rate,
                                loss_reduction = best_xgb$loss_reduction ,sample_size = best_xgb$sample_size),
+    ## Control
     null_model = null_model),
   cross = TRUE)
 
@@ -380,12 +550,143 @@ model_selection_func <- function(df_collect_metrics, model_of_interest, model_re
 }
 
 # Pick Models to tune
+
+# Bag Mars
+model_selection_func(
+  df_collect_metrics = model_select_results,
+  model_of_interest = "bag_mars",
+  model_resamples = model_select_resamples
+)
+
+# XGB
+model_selection_func(
+  df_collect_metrics = model_select_results,
+  model_of_interest = "boost_tree",
+  model_resamples = model_select_resamples
+)
+
+# Mlp
 model_selection_func(
   df_collect_metrics = model_select_results,
   model_of_interest = "mlp",
   model_resamples = model_select_resamples
 )
 
-# Back to default (1 core)
-plan(sequential)  
+
+#### Selected Models Tuning ####
+
+# Parameters for tuning
+resamples <- vfold_cv(data = validation_set,v = 10,strata = yield)
+metrics <- metric_set(mae)
+control <- control_grid(verbose = TRUE,save_workflow = TRUE,save_pred = TRUE,allow_par = TRUE)
+
+# SA Control
+control_sim_anneal = control_sim_anneal(
+  verbose = TRUE,
+  no_improve = 20,            # More patience before restart
+  restart = 10,                # Allow restarts to escape local minimal
+  radius = c(0.15,0.30),      # Encourage larger jumps (default is 0.1)
+  cooling = 0.05,                # Faster temperature decay = more exploration early on
+)
+
+# BO Control
+control_bo <- control_bayes(
+  verbose = TRUE,
+  verbose_iter = TRUE,
+  no_improve = 30,
+  save_workflow = TRUE,save_pred = TRUE,
+  seed = 123,
+  uncertain = 20)
+
+## Bag Mars Model
+# Model Spec
+bag_mars_tune <- bag_mars(
+  num_terms = tune(),
+  prod_degree = tune()) %>%
+  set_engine("earth",penalty = tune()) %>%
+  set_mode("regression")
+
+# Create a LHC grid
+bag_mars_grid <- grid_space_filling(
+    num_terms(range = c(2, 100)),
+    prod_degree(range = c(1, 4)),
+    penalty(range = c(-10, 0)),
+    size = 60)
+
+# Create a workflow
+bag_mars_tune_workflow <- workflow()%>%
+  add_model(bag_mars_tune) %>%
+  add_recipe(pca_4_final)
+
+# Tune the model
+bag_mars_initial <- bag_mars_tune_workflow %>%
+  tune_grid(
+    resamples = resamples,
+    grid = bag_mars_grid,
+    metrics = metrics,
+    control = control
+  )
+
+# Extract the best results
+bag_mars_initial_best <- bag_mars_initial %>% select_best()
+show_best(bag_mars_initial)
+
+# Update the workflow
+bag_mars_tune_workflow <- bag_mars_tune_workflow %>% finalize_workflow(bag_mars_initial_best)
+
+### Optim SA ###
+
+## SA 
+sa_optim <- bag_mars_tune_workflow %>%
+  tune_sim_anneal(
+    resamples = resamples,
+    metrics = metrics,
+    initial = bag_mars_initial,
+    iter = 100,
+    control = control_sim_anneal
+  )
+  
+plan(sequential) 
+
+## MLP
+# Model Spec
+mlp_tuned <- 
+  mlp(
+    hidden_units = tune(),
+    penalty = tune(),
+    learn_rate = tune(),
+    epochs = tune(),
+    activation = "relu"
+  ) %>%
+  set_engine("brulee",momentum = tune()
+    ) %>%
+  set_mode("regression")
+
+# Create LHC Grid
+mlp_grid <- grid_space_filling(
+  hidden_units(range = c(16,256)),
+  penalty(range = c(-10,0)),
+  learn_rate(range = c(-10,-1)),
+  epochs(range = c(10,250)),
+  momentum(range = c(0,1)),
+  size = 50)
+
+# Create  Workflow
+mlp_worklow_tune <- workflow() %>%
+  add_model(mlp_tuned)%>%
+  add_recipe(pca_4_final)
+
+# Tune MLP
+tune_mlp <- mlp_worklow_tune %>%
+  tune_grid(
+    resamples = resamples,
+    grid = mlp_grid,
+    metrics = metrics,
+    control = control
+  )
+
+
+
+
+
 
